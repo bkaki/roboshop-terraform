eb7fd1d1033d0096c8e74562c4b2cc5a3a48c398
bkaki <bhaskarkakii77@gmail.com>
2023-09-27 16:20:47 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,9 +1,111 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "instance" {
   ami           = "ami-03265a0778a880afb"
 
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
 }
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+
+resource "aws_instance" "instance" {
+  ami           = "ami-03265a0778a880afb"
+
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+
+
+
